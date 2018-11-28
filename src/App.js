ef4484e2047e625b9791ddbf18958da869cce032
laiiihz <laiiihz@outlook.com>
2018-11-28 15:58:42 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import M from 'materialize-css';
+//import logo from './logo.svg';
 import './App.css';
+
+const webName='Laihz';
+const webNavName='Laihz Navigator';
+
+class NavBar extends Component{
+    render() {
+        return (
+            <div>
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="#" className="brand-logo right">Logo</a>
+                        <ul id="nav-mobile" className="left ">
+                            <li><a href="sass.html">Sass</a></li>
+                            <li><a href="badges.html">Components</a></li>
+                            <li><a href="collapsible.html">JavaScript</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <NavBar/>
       </div>
     );
   }
