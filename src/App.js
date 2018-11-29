e566bc7931bf568c3acb52cf8f9e97ffb49130bf
laiiihz <laiiihz@outlook.com>
2018-11-29 22:16:34 +0800

    add webs

--- src/App.js
+++ src/App.js
@@ -9,6 +9,55 @@ const webNavName='Laihz Navigator';
 /*
 * web
 * */
+const laihzGithub='https://github.com/laiiihz/';
+
+const websites={
+    DeveloperWeb:{
+        android:'http://developers.android.google.cn/',
+        vscode:'https://code.visualstudio.com/',
+        vside:'https://visualstudio.microsoft.com/',
+        jetbrains:'https://www.jetbrains.com/',
+        stackOverFlow:'https://stackoverflow.com',
+        github:'https://github.com/'
+    }
+
+};
+/*TODO some class to const*/
+
+class Design {
+    static unsplash='https://unsplash.com/';
+    static iconFont='http://www.iconfont.cn/';
+
+}
+
+class Website {
+    static otpTool='https://otp.landian.la/zh-cn/';
+    static appinn='https://www.appinn.com/';
+    static iplaysoft='https://www.iplaysoft.com/';
+    static coolapk='http://www.coolapk.com/';
+    static zimuzu='http://www.zimuzu.tv/';
+}
+
+class OJ {
+    static PAT='https://www.patest.cn/';
+    static leetcodeCN='https://www.leetcode-cn.com/';
+    static luogu='https://www.luogu.org/';
+}
+
+class spirit {
+    static offTheAir='https://www.adultswim.com/videos/off-the-air';
+    static chromeEx='https://experiments.withgoogle.com/';
+}
+
+class Goods{
+    static ikea='https://www.ikea.cn/cn/zh/';
+    static decathlon='https://www.decathlon.com.cn/';
+    static mi='https://www.mi.com/';
+}
+
+function ListHref(props) {
+    return <li><a href={props.webName}>{props.webName}</a></li>;
+}
 
 class NavBar extends Component{
     render() {
@@ -18,8 +67,8 @@ class NavBar extends Component{
                     <div className="nav-wrapper">
                         <a href="#" className="brand-logo right">Logo</a>
                         <ul id="nav-mobile" className="left ">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
+                            <ListHref webName='laihzGithub'/>
+                            <li><a href={DeveloperWeb.android}>Components</a></li>
                             <li><a href="collapsible.html">JavaScript</a></li>
                         </ul>
                     </div>
